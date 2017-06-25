2b137b92e6a67debd2b11391fe0ccc3260d7a223
Jesse Rosalia <jesse.rosalia@Jesses-MacBook-Air.local>
2017-06-25 15:16:59 -0700

    Removed experiments and generated new express project

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-const express = require('express');
-const actions = require('./routes/actions');
-const app = express();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.use('/pages', express.static('public/html'));
+var index = require('./routes/index');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.get('/app/:message', function (req, res) {
-  res.send('Got a message: ' + req.params["message"]);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.use('/actions', actions);
-
-app.use(function(req, res) {
-  res.status(404).send('Oh my! Did you type in the wrong page?');
-});
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Server started');
-});
+module.exports = app;
