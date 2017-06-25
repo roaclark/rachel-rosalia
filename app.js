74ddca6ce7d169a08a5e4281f34b63973d6844da
Jesse Rosalia <jesse.rosalia@Jesses-MacBook-Air.local>
2017-06-25 13:56:05 -0700

    Added more endpoints

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.get('/app/:message', function (req, res) {
+  res.send('Got a message: ' + req.params["message"]);
+});
+
+app.use(function(req, res) {
+  res.status(404).send('Oh my! Did you type in the wrong page?');
+});
 
 app.listen(3000, function () {
-  console.log('Listening on port 3000')
-})
+  console.log('Listening on port 3000');
+});
