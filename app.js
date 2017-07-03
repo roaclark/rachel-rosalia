ca84066dde0d1a40241d3db8dadf174e472c2e2a
Rachel Rosalia <rachel@rosalia.me>
2017-07-03 12:54:05 -0700

    Adding a public directory for the client

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/', index);
 
 // catch 404 and forward to error handler
