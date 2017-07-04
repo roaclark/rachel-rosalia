a6910ac16be2fb84d52f83cb945061112fb076fc
Rachel Rosalia <rachel@rosalia.me>
2017-07-04 16:16:46 -0700

    Added resume

--- client/src/content/work/WorkContent.js
+++ client/src/content/work/WorkContent.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
+import "./WorkContent.css"
 
-class WorkContent extends Component {
+class Resume extends Component {
+  render() {
+    return <embed id="resume" src="/RachelRosaliaResume.pdf" />;
+  }
+}
+
+class BasicWorkContent extends Component {
   render() {
     return (
       <div>
         <h1>My Work History</h1>
-        <p>This page is still in development. For now please request my resume.</p>
+        <p>This page is still in development. For now please view my <Link to="/work/resume">resume</Link>.</p>
       </div>
+    );
+  }
+}
+
+class WorkContent extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/work" component={BasicWorkContent}/>
+        <Route exact path="/work/resume" component={Resume}/>
+      </Switch>
     );
   }
 }
